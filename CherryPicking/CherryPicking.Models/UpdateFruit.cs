b27d05de67c93538ae4d049c45ad4491c4296eae
Sam Smith <samsmithnz@gmail.com>
2020-02-25 09:44:32 -0500

    Added Update for Branch C

---
+++ CherryPicking/CherryPicking.Models/UpdateFruit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CherryPicking.Models
+{
+    class UpdateFruit
+    {
+
+        //Filler class to simulate updating.
+    }
+}
