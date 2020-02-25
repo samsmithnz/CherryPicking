6d97f4c1fea7a665bd7cdde892d8b35436c6e158
Sam Smith <samsmithnz@gmail.com>
2020-02-25 08:21:13 -0500

    Added delete

---
+++ CherryPicking/CherryPicking.Models/DeleteFruit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CherryPicking.Models
+{
+    class DeleteFruit
+    {
+
+        //Delete fruit filler class
+    }
+}
